dbdf81797f83779cf367555ac635f729ec10f0b3
sonyayahab <sonyayahab@gmail.com>
2017-09-11 22:41:42 +0700

    Create lab01a.py
    a simple program task that use print function and input method to print to the screen what input from user.

---
+++ lab01/lab01a.py
@@ -0,0 +1,5 @@
+nama = input("Masukkan nama Anda: ");
+tahun = input("Masukkan tahun lahir Anda: ");
+tinggal = input("Masukkan tempat tinggal Anda Sekarang: ");
+
+print (nama, "lahir tahun", tahun, "tinggal di", tinggal);
